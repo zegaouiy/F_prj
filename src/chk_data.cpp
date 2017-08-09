d987b34801b55f9516032024d97641c207269960
zegaouiy <younes.zegaoui@etu.umontpellier.fr>
2017-08-09 20:24:29 +0200

    debut chk data

---
+++ src/chk_data.cpp
@@ -0,0 +1,20 @@
+void read_map(int* map, char* nom, int n)
+{
+  FILE *f_image;
+  int  nb_colonnes, nb_lignes, max_grey_val;
+  taille_image=3*taille_image;
+   
+  if( (f_image = fopen(nom, "rb")) == NULL)
+    {
+      printf("\nPas d'acces en ecriture sur l'image %s \n", nom);
+      exit(EXIT_FAILURE);
+    }
+   
+  if( (fread((int*)map, sizeof(int), n, f_image))
+      != (int)(n))
+    {
+      printf("\nErreur d'ecriture de l'image %s \n", nom);
+      exit(EXIT_FAILURE);
+    }
+  fclose(f_image);
+}
