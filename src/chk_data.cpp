26f0713a1abcefa9a91b7a544404fcdc32548116
zegaouiy <younes.zegaoui@etu.umontpellier.fr>
2017-08-21 18:05:51 +0200

    chk data adv

--- src/chk_data.cpp
+++ src/chk_data.cpp
@@ -27,3 +27,59 @@ void count_window(vector<int> win, ofstream count, int i)
     }
 }
 
+void count_gt(OCTET* gt, int h, int w)
+{
+  int  true_n, i, j, size, voisin, mi, mj, p_i, p_j, lab = 1, n = h * w;
+  vector<int> done;
+  OCTET* out, t;
+  float adv = 0;
+
+  allocation_tableau(out, OCTET, n);
+
+  for(i = 0; i < n; i++)
+    out[i] = 0;
+
+  i = 0;
+  mi = -1;
+  size = 0;
+
+  while(i < h)
+    {
+      j = 0;
+      while( j < w)
+	{
+	  if(gt[i * w + j] > 0)
+	    {
+	      done.clear();
+	      done.push_back(i);
+	      done.push_back(j);
+	      size++;
+
+	      while(!done.empty())
+		{
+		  p_j = done.back();
+		  done.pop_back();
+		  p_i = done.back();
+		  done.pop_back();
+		
+	      for(i = p_i - 1; i <= p_i +1; i++)
+		for(j = p_j - 1; j <= p_j +1; j++)
+		  {
+		    voisin = i * w + j;
+		    if(mask[i*w + j] && map[voisin] == 0 && out[voisin] != 0)
+		      {
+			done.push_back(i);
+			done.push_back(j);
+			  
+			map[i * w + j] = lab;
+			size++;
+		      }
+		  }
+	    }
+	  sizes.push_back(size);
+	  lab++;
+	  adv += size;
+	  maxi(img, map, h, w, mi, mj);
+	  //cout << " adv = " << adv/(float)true_n * 100.0 << endl;
+	}
+    }
